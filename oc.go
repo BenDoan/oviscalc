1b7eb45b8ab69c953684c91e57c09e4e63bac6e8
Ben Doan <ben@bendoan.me>
2014-02-19 21:39:55 -0600

    now outputs duration between two times

--- oc.go
+++ oc.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"github.com/docopt/docopt.go"
-	"time"
+    "time"
 )
 
 func main() {
-	usage := "Usage: oc add <time> <duration>"
+	usage := "Usage: oc add <time> <time>"
 	args, _ := docopt.Parse(usage, nil, true, "", false)
 	fmt.Println(args)
 
-	fmt.Println(time.Parse("15:04", "2:03"))
+    times := args["<time>"].([]string)
+
+    time1, _ := time.Parse("15:04", times[0])
+    time2, _ := time.Parse("15:04", times[1])
+
+    fmt.Println(time2.Sub(time1))
 }
