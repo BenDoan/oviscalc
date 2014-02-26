3cd3007cbfe3ddd1e330e447cafa3691f441ff56
Ben Doan <ben@bendoan.me>
2014-02-25 22:37:10 -0600

    reformated code

--- oc.go
+++ oc.go
@@ -7,16 +7,20 @@ import (
 )
 
 func main() {
-	usage := "Usage: oc add <time> <time>"
+	usage := "Usage: oc [diff <time> <time>]"
 	args, _ := docopt.Parse(usage, nil, true, "", false)
 
-	times := args["<time>"].([]string)
+	if args["diff"].(bool) {
+		diff(args["<time>"].([]string))
+	}
+}
 
+func diff(times []string) {
 	time1, err := time.Parse("15:04", times[0])
 	time2, err2 := time.Parse("15:04", times[1])
 
 	if err == nil && err2 == nil {
-		fmt.Println(time1.Sub(time2))
+		fmt.Println(TimeAbs(time1.Sub(time2)))
 	} else {
 		if err != nil {
 			fmt.Println(err)
@@ -26,5 +30,12 @@ func main() {
 			fmt.Println(err)
 		}
 	}
+}
 
+func TimeAbs(dur time.Duration) time.Duration {
+	if dur > 0 {
+		return dur
+	} else {
+		return -1 * dur
+	}
 }
